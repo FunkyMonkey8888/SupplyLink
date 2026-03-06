992f5f7c2959ff99d5252cc6392467965ea619bc
FunkyMonkey8888 <dummy@gmail.com>
2026-03-06 11:47:26 +0000

    Day 14

--- server/src/main/java/com/edutech/progressive/entity/Product.java
+++ server/src/main/java/com/edutech/progressive/entity/Product.java
@@ -11,7 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+// import com.fasterxml.jackson.annotation.JsonIgnore;
 
 // import org.hibernate.annotations.Table;
 
@@ -85,8 +85,6 @@ public class Product {
     }
     public Product() {
     }
-
-    
 
     
 
